698732cf0936683727944ca09954fa48e7c0d194
JrDevCJ <109694054+JrDevCJ@users.noreply.github.com>
2022-07-26 16:31:19 -0300

    Create removerEspacosEmBrancoString.py

---
+++ utils/removerEspacosEmBrancoString.py
@@ -0,0 +1,10 @@
+string = '  String com espaços  '
+
+print(f'String sem tratamento =\'{string}\'')
+
+print(f'String após remover os espaços com (lstrip) =\'{string.lstrip()}\'')
+
+print(f'String após remover os espaços do início com (lstrip) =\'{string.strip()}\'')
+
+print(f'String após remover os espaços do final com (rstrip)  =\'{string.rstrip()}\'')
+
